5726b590a1af74a4fd6d3e9e004edbcb325ca382
Doug Wells <dougwells@gmail.com>
2016-03-06 13:46:21 -0700

    /campgrounds page basic functionality.  Name & image

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require("express");
 var app = express();
-var sites =["Yosemite", "Zions", "Yellowstone"];
+var campgrounds =[{name:"Yosemite", image:"https://farm8.staticflickr.com/7530/15570594753_49fe0b95da.jpg"}, {name:"Zions", image:"https://farm7.staticflickr.com/6014/6193268729_7d1deddba3.jpg"}, {name:"Yellowstone", image:"https://farm5.staticflickr.com/4141/4814814652_a6c1c9cbfe.jpg"}];
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-   res.render("home"); 
-    console.log("Developers ... YelpCamp is hiring.  Contact us today!");
+    res.render("landing"); 
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {sites:sites});
+    res.render("campgrounds", {campgrounds:campgrounds});
 
 })
 

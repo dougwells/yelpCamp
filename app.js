a47e3b002e9a73340331f07d6b4800a6284b157b
Doug Wells <dougwells@gmail.com>
2016-03-07 08:26:30 -0700

    CSS Styling for displaying list of campgrounds

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var campgrounds =[{name:"Yosemite", image:"https://farm8.staticflickr.com/7530/15570594753_49fe0b95da.jpg"}, {name:"Zions", image:"https://farm7.staticflickr.com/6014/6193268729_7d1deddba3.jpg"}, {name:"Yellowstone", image:"https://farm5.staticflickr.com/4141/4814814652_a6c1c9cbfe.jpg"}];
+var campgrounds =[{name:"Yosemite", image:"https://farm8.staticflickr.com/7530/15570594753_49fe0b95da.jpg"}, {name:"Zions Park", image:"https://farm7.staticflickr.com/6014/6193268729_7d1deddba3.jpg"}, {name:"Yellowstone", image:"https://farm5.staticflickr.com/4141/4814814652_a6c1c9cbfe.jpg"}];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));

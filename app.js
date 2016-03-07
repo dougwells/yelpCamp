77bf6e51226138652eac5dd79a34d94483eec87a
Doug Wells <dougwells@gmail.com>
2016-03-06 17:46:23 -0700

    Added Create New Campground Form & related POST request w/redirect

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var campgrounds =[{name:"Yosemite", image:"https://farm8.staticflickr.com/7530/15570594753_49fe0b95da.jpg"}, {name:"Zions", image:"https://farm7.staticflickr.com/6014/6193268729_7d1deddba3.jpg"}, {name:"Yellowstone", image:"https://farm5.staticflickr.com/4141/4814814652_a6c1c9cbfe.jpg"}];
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -12,7 +14,16 @@ app.get("/", function(req, res){
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds:campgrounds});
 
-})
+});
+
+app.post("/campgrounds", function(req, res){
+   campgrounds.unshift({name:req.body.name, image:req.body.image});
+   res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+   res.render("new.ejs"); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Full Speed Ahead!!!");

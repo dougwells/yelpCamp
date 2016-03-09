8e849326fd3b8529f635260cc4d9bf043ffc93bc
Doug Wells <dougwells@gmail.com>
2016-03-09 12:59:11 -0700

    /new.  Added ability to input new campgrounds

--- v2/app.js
+++ v2/app.js
@@ -15,27 +15,6 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-//     {name: "Granite Hill", 
-//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
-        
-//     },function(err,campground){
-//         if(err){
-//             console.log("Error: ", err);
-//         }else{
-//              console.log("new campground")
-//             console.log(campground);
-//         }
-//     });
-// 
-// var campgrounds = [
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-//         ]
-
-
-    
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -55,14 +34,23 @@ app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var newCampground = {name: name, image: image};
+    
+    Campground.create(newCampground, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    res.send("Show Page --Comming Soon!")
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
